e2f8cfb5cdf8d25b5dfa9b04a5eac5b414f96f32
AbdullahDev111 <siddquiabdullah652@gmail.com>
2026-05-15 11:39:56 +0530

    datatypes in javascript

---
+++ DAY 1/datatype.js
@@ -0,0 +1,32 @@
+// Primitivedatatypes
+
+console.log(typeof 2)
+console.log(typeof -3)
+console.log(typeof "adfasfasdf")
+console.log(typeof -"100")
+console.log(typeof +"100")
+console.log(typeof fdjdrjfnode)
+console.log(typeof +"100Anurag")
+console.log(typeof NaN)
+
+
+//to conver string to number
+console.log(parseInt("100"))
+console.log( typeof parseInt("100"))
+
+console.log(parseInt("100retwerewrr"))
+
+// boolean
+
+console.log(typeof true)
+console.log(typeof false)
+console.log(typeof +true)
+console.log(+true)
+console.log(typeof +false)
+console.log(+false)
+
+
+// undefined -> In JavaScript, undefined means “a variable exists, but it has no value assigned”.
+// It’s both a primitive value and a type.
+
+// null-> The null keyword refers to the null primitive value, which represents the intentional absence of any object value.
